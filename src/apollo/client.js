937547e7bd8c75fb09b862d9faff31b018dd66ba
Conor <conor@wilxite.com>
2019-06-10 14:38:07 +0100

    ApolloClient gets token from user in redux store

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,33 @@
-import ApolloClient from "apollo-boost"
+import ApolloClient from "apollo-client"
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-client-preset'
 import fetch from "isomorphic-fetch"
+import store from './redux/store'
 
-export const client = new ApolloClient({
-    uri: "/.netlify/functions/graphql",
+const uri = "/.netlify/functions/graphql"
+
+const httpLink = createHttpLink({
+    uri,
+    credentials: 'include'
+})
+
+const authLink = setContext((_, { headers }) => {
+    let token = null
+    const { user } = store.getState().user
+    if (user) token = user.token
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ""
+        }
+    }
+})
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
     fetch
 })
+
+export default client
